2f54a8872c970904ca861e9c2553f35c1299cd42
Vaibhav Biturwar <40133271+VaibhavBiturwar@users.noreply.github.com>
2023-09-24 14:10:33 +0000

    variables/ dataTypes/ conversionOperations

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+'use-strict' // to treat all js as per the new version | use by-default
+
+// console.log(3+3) console.log("Vaibhav Biturwar"); // gives error | new statement should start with a new line
+// console.log(3+3); console.log("Vaibhav Biturwar"); // this works
+
+// Data Types
+// number=> 2 - Power 53
+// bigInt
+// string => ""
+// boolean =>true/false
+// null => stand alone value | representation of an empty value
+// undefined => value is not available | data type is not defined
+// symbol => to find uniqueness 
+
+// object => {}
+
+console.table([typeof null, typeof undefined]); // object | undefined
+
