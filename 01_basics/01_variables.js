0eab6396f0539a4a4a12ecd70605aa74929596a4
Vaibhav Biturwar <40133271+VaibhavBiturwar@users.noreply.github.com>
2023-09-24 15:12:04 +0000

    spell check

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,11 +6,16 @@ let accountState;
 // accountId =2 // cannot be updated because declared as constant;
 accountEmail = "zxc@xc.com";
 accountPass = "123edc";
-accountCity="Raipur";
-console.table([accountId,accountEmail,accountPass,accountCity, accountState])
+accountCity = "Raipur";
+console.table([
+  accountId,
+  accountEmail,
+  accountPass,
+  accountCity,
+  accountState,
+]);
 
 /*
 var-> global scope / avoid using
 let-> works in blocked scope / to be used on priority
 */
-
--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,22 +1,21 @@
 let score = 3;
 
 console.log(typeof score);
-// console.log(typeof(score));// correct | used as a function 
+// console.log(typeof(score));// correct | used as a function
 
 // number conversion
 let scoreInNum = Number(score);
 console.log(typeof scoreInNum);
 
-score = "33A" // should not be converted to number since it contains a character in it.
+score = "33A"; // should not be converted to number since it contains a character in it.
 console.log(typeof score); // string
 scoreInNum = Number(score); // value is NnN
 console.table([typeof scoreInNum, scoreInNum]); // number
 
 // !!!!!
-// On coverting a string to a number, 
+// On converting a string to a number,
 // check if the value is NaN(Not a number) before making any further calculations.
 // !!!!!
 
-
 const z = 33;
-console.table([Number(z), String(z), Boolean(z)])
+console.table([Number(z), String(z), Boolean(z)]);
