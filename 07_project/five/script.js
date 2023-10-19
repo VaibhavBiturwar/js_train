1fd55710849842a33c20ce500d0f40628307bc98
Vaibhav Valere <vaibhav.valere@gmail.com>
2023-10-19 18:13:56 +0530

    proj five adv colors

--- 07_project/five/script.js
+++ 07_project/five/script.js
@@ -4,7 +4,9 @@ let intervalObj = undefined;
 document.querySelector("#start").addEventListener("click", () => {
   console.log("start");
   if (intervalObj == undefined) {
-    intervalObj = setInterval(changeColor, 500);
+    // intervalObj = setInterval(changeColor, 500);
+    // intervalObj = setInterval(changeColorHex, 500);
+    intervalObj = setInterval(changeColorAlpha, 500);
   } else {
     console.log("Changer already active");
   }
@@ -16,15 +18,32 @@ document.querySelector("#stop").addEventListener("click", () => {
   bodyObj.style.backgroundColor = "grey";
 });
 
-const getR = () => {
-  var randomNumber = Math.floor(Math.random() * 256);
+const getR = (val) => {
+  var randomNumber = Math.floor(Math.random() * val);
   return randomNumber;
 };
 
 const changeColor = () => {
-  const c1 = getR();
-  const c2 = getR();
-  const c3 = getR();
+  const c1 = getR(256);
+  const c2 = getR(256);
+  const c3 = getR(256);
   // console.log(c1, c2, c3);
   bodyObj.style.backgroundColor = `rgba(${c1},${c2},${c3},1)`;
+};
+
+const changeColorHex = () => {
+  const hex = "0123456789ABCDEF";
+  let hexCode = "#";
+  for (let i = 0; i < 6; i++) {
+    hexCode += hex[getR(16)];
+  }
+  bodyObj.style.backgroundColor = hexCode;
+};
+
+const changeColorAlpha = () => {
+  const c1 = getR(256);
+  const c2 = getR(256);
+  const c3 = getR(256);
+  const c4 = Math.random();
+  bodyObj.style.backgroundColor = `rgba(${c1},${c2},${c3},${c4})`;
 };
